cafd29d578517e5303c81e2108070dda48cd944d
Ashish Pradhan <ashishpradhan2345@gmail.com>
2021-09-18 23:47:03 +0530

    integration

--- Frontend/src/components/Card/Cards.js
+++ Frontend/src/components/Card/Cards.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React ,{useState} from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -7,6 +7,17 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function ImgMediaCard(props) {
+
+
+  const [likecounter,setLikeCounter] = useState(props.likes)
+  function handleLike() {
+    fetch(`http://localhost:300/course/${props.id}/like`, {
+      method: "POST",
+    }).then(() => {
+      setLikeCounter((preLike) => preLike + 1);
+    });
+  }
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia
@@ -19,13 +30,16 @@ export default function ImgMediaCard(props) {
         <Typography gutterBottom variant="h5" component="div">
           {props.title}
         </Typography>
+        <Typography gutterBottom variant="h6" component="div">
+          {props.author}
+        </Typography>
         <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
+          {props.body}
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Likes</Button>
+        <Button size="small" onClick={handleLike}>Likes</Button>
+        <p>{likecounter}</p>
         <Button size="small">Read More</Button>
       </CardActions>
     </Card>
